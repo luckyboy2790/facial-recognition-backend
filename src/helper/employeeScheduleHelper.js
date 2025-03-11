cdc8591c9c45e01d58fa0e6d258e5895d95a62ad
luckyboy2790 <rorifloris78@gmail.com>
2025-03-11 04:11:54 +0000

    feat: complete delete schedule

--- src/helper/employeeScheduleHelper.js
+++ src/helper/employeeScheduleHelper.js
@@ -3,7 +3,12 @@ import moment from 'moment';
 export const convertTo12HourFormat = (timeString) => {
   if (!timeString) return '';
 
-  const date = new Date(`1970-01-01T${timeString}`);
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+
+  const date = new Date(`${year}-${month}-01T${timeString}`);
+
   return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', hour12: true });
 };
 
--- src/routes/schedule.route.js
+++ src/routes/schedule.route.js
@@ -5,11 +5,13 @@ const {
   getSchedule,
   getScheduleDetail,
   updateScheduleDetail,
+  deleteSchedule,
 } = require('../controllers/schedule.controller');
 
 router.post('/create_schedule', createSchedule);
 router.get('/get_schedule', getSchedule);
 router.get('/get_schedule/:id', getScheduleDetail);
 router.post('/update_schedule/:id', updateScheduleDetail);
+router.post('/delete_schedule', deleteSchedule);
 
 module.exports = router;
