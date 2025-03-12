65216b4dc9ad812a0095c53b17af5470ae1f2e0e
luckyboy2790 <rorifloris78@gmail.com>
2025-03-12 01:50:36 +0000

    feat: complete update attendance

--- src/controllers/attendance.controller.js
+++ src/controllers/attendance.controller.js
@@ -11,6 +11,12 @@ exports.createAttendance = async (req, res) => {
 
     const existEmployee = await AttendanceModel.findOne({ employee: employee, date: date });
 
+    if (time_in === '') {
+      return res.status(500).json({
+        message: `You have to input time in.`,
+      });
+    }
+
     if (existEmployee) {
       return res.status(400).json({
         message: `The employee already clocked in today at ${existEmployee.time_in}`,
@@ -32,9 +38,17 @@ exports.createAttendance = async (req, res) => {
 
     const { start_time, off_time, rest_days } = employeeSchedule;
 
+    const dayName = moment(date, 'YYYY-MM-DD').format('dddd');
+
     if (rest_days.includes(date)) {
       return res.status(400).json({
         message: 'Today is a rest day for this employee.',
+      });
+    }
+
+    if (rest_days.includes(dayName)) {
+      return res.status(400).json({
+        message: `This day (${dayName}) is a rest day for this employee.`,
       });
     }
 
@@ -80,7 +94,7 @@ exports.createAttendance = async (req, res) => {
       total_hours = totalHours.toFixed(1);
     }
 
-    const newSchedule = new AttendanceModel({
+    const newAttendance = new AttendanceModel({
       employee,
       date,
       time_in: formattedTimeIn,
@@ -92,9 +106,9 @@ exports.createAttendance = async (req, res) => {
       comment: null,
     });
 
-    const scheduleId = await newSchedule.save();
+    const attendanceId = await newAttendance.save();
 
-    res.status(200).json({ message: 'Schedule created successfully', schedule: scheduleId });
+    res.status(200).json({ message: 'Schedule created successfully', attendance: attendanceId });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error.message });
@@ -202,5 +216,125 @@ exports.getAttendance = async (req, res) => {
   } catch (error) {
     console.error('Error getting attendance records:', error);
     res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+exports.getAttendanceDetail = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const attendance = await AttendanceModel.findById(id);
+
+    res.status(200).json({
+      message: 'Attendance detail fetched successfully',
+      attendance,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateAttendance = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { id } = req.params;
+
+    const { employee, date, time_in, time_out, reason } = req.body;
+
+    if (time_in === '') {
+      return res.status(500).json({
+        message: `You have to input time in.`,
+      });
+    }
+
+    const employeeSchedule = await EmployeeScheduleModel.findOne({
+      employee: employee,
+      status: 'Present',
+      from: { $lte: date },
+      to: { $gte: date },
+    });
+
+    if (!employeeSchedule) {
+      return res.status(400).json({
+        message: 'No active schedule found for this employee.',
+      });
+    }
+
+    const { start_time, off_time, rest_days } = employeeSchedule;
+
+    const dayName = moment(date, 'YYYY-MM-DD').format('dddd');
+
+    if (rest_days.includes(date)) {
+      return res.status(400).json({
+        message: 'Today is a rest day for this employee.',
+      });
+    }
+
+    if (rest_days.includes(dayName)) {
+      return res.status(400).json({
+        message: `This day (${dayName}) is a rest day for this employee.`,
+      });
+    }
+
+    const formattedTimeIn = time_in
+      ? moment(`${date} ${time_in}`).format('YYYY-MM-DD hh:mm:ss A')
+      : '';
+    const formattedTimeOut = time_out
+      ? moment(`${date} ${time_out}`).format('YYYY-MM-DD hh:mm:ss A')
+      : '';
+
+    let status_timein = '';
+    if (time_in) {
+      const timeInMoment = moment(`${date} ${time_in}`, 'YYYY-MM-DD HH:mm:ss');
+      const startTimeMoment = moment(`${date} ${start_time}`, 'YYYY-MM-DD HH:mm:ss');
+
+      if (timeInMoment.isAfter(startTimeMoment)) {
+        status_timein = 'Late In';
+      } else {
+        status_timein = 'In Time';
+      }
+    }
+
+    let status_timeout = '';
+    if (time_out) {
+      const timeOutMoment = moment(`${date} ${time_out}`, 'YYYY-MM-DD HH:mm:ss');
+      const offTimeMoment = moment(`${date} ${off_time}`, 'YYYY-MM-DD HH:mm:ss');
+
+      if (timeOutMoment.isBefore(offTimeMoment)) {
+        status_timeout = 'Early Out';
+      } else {
+        status_timeout = 'On Time';
+      }
+    }
+
+    let total_hours = '';
+    if (time_in && time_out) {
+      const timeInMoment = moment(`${date} ${time_in}`, 'YYYY-MM-DD HH:mm:ss');
+      const timeOutMoment = moment(`${date} ${time_out}`, 'YYYY-MM-DD HH:mm:ss');
+
+      const duration = moment.duration(timeOutMoment.diff(timeInMoment));
+      const totalHours = duration.asMinutes() / 60;
+
+      total_hours = totalHours.toFixed(1);
+    }
+
+    const scheduleId = await AttendanceModel.findByIdAndUpdate(id, {
+      employee,
+      date,
+      time_in: formattedTimeIn,
+      time_out: formattedTimeOut,
+      total_hours,
+      status_timein,
+      status_timeout,
+      reason: reason,
+      comment: null,
+    });
+
+    res.status(200).json({ message: 'Schedule created successfully', schedule: scheduleId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
   }
 };
--- src/routes/attendance.route.js
+++ src/routes/attendance.route.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { createAttendance, getAttendance } = require('../controllers/attendance.controller');
+const {
+  createAttendance,
+  getAttendance,
+  getAttendanceDetail,
+  updateAttendance,
+} = require('../controllers/attendance.controller');
 
 router.post('/create_attendance', createAttendance);
 router.get('/get_attendance', getAttendance);
+router.get('/get_attendance/:id', getAttendanceDetail);
+router.post('/update_attendance/:id', updateAttendance);
 
 module.exports = router;
