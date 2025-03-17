4d882951bb263e4b75637b49b950911055169f07
luckyboy2790 <rorifloris78@gmail.com>
2025-03-17 04:50:32 +0000

    feat: complete update role function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,3 +34,25 @@ exports.getRole = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.updateRole = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { id } = req.params;
+
+    const { name, status, accessRight } = req.body;
+
+    await UserRoleModel.findByIdAndUpdate(id, {
+      name: name,
+      status: status,
+      accessRight: accessRight,
+    });
+
+    res.status(200).json({ message: 'Update successfully' });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: error });
+  }
+};
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { signup, signin } = require('../controllers/auth.controller.js');
-const { createRole, getRole } = require('../controllers/user.controller.js');
+const { createRole, getRole, updateRole } = require('../controllers/user.controller.js');
 
 router.post('/register', signup);
 router.post('/login', signin);
 
 router.post('/create_role', createRole);
 router.get('/get_role', getRole);
+router.post('/update_role/:id', updateRole);
 module.exports = router;
