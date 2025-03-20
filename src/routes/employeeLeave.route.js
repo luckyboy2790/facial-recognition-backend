5e8740cd419a532626140dd330bbe241c1bdf51b
luckyboy2790 <rorifloris78@gmail.com>
2025-03-20 17:02:09 +0000

    feat: add edit leave endpoint

--- src/routes/employeeLeave.route.js
+++ src/routes/employeeLeave.route.js
@@ -4,18 +4,20 @@ const {
   createEmployeeLeave,
   getPersonalEmployeeLeave,
   getPersonalEmployeeLeaveDetail,
-  updateEmployeeLeave,
+  updatePersonalEmployeeLeave,
   deleteEmployeeLeave,
   getEmployeeLeave,
+  updateEmployeeLeave,
 } = require('../controllers/employeeLeave.controller');
 const verifyToken = require('../middlewares/authJWT');
 
 router.post('/personal/create_leave', verifyToken, createEmployeeLeave);
 router.get('/personal/get_personal_leaves', verifyToken, getPersonalEmployeeLeave);
 router.get('/personal/get_personal_leave/:id', verifyToken, getPersonalEmployeeLeaveDetail);
-router.post('/personal/update_leave/:id', verifyToken, updateEmployeeLeave);
+router.post('/personal/update_leave/:id', verifyToken, updatePersonalEmployeeLeave);
 router.post('/personal/delete_leave', verifyToken, deleteEmployeeLeave);
 
 router.get('/get_leaves', verifyToken, getEmployeeLeave);
+router.post('/update_leave/:id', verifyToken, updateEmployeeLeave);
 
 module.exports = router;
