80891f01b32ebc3198452dcadab52e2d9b5c2c55
luckyboy2790 <rorifloris78@gmail.com>
2025-03-20 06:37:29 +0000

    feat: add get leave endpoint

--- src/routes/employeeLeave.route.js
+++ src/routes/employeeLeave.route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createEmployeeLeave } = require('../controllers/employeeLeave.controller');
+const {
+  createEmployeeLeave,
+  getPersonalEmployeeLeave,
+} = require('../controllers/employeeLeave.controller');
 const verifyToken = require('../middlewares/authJWT');
 
 router.post('/personal/create_leave', verifyToken, createEmployeeLeave);
+router.get('/personal/get_personal_leaves', verifyToken, getPersonalEmployeeLeave);
 
 module.exports = router;
