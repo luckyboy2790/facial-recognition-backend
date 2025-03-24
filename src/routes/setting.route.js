bf4c03d0bed7ed07fbccc289bac5b48c61d0dd5b
luckyboy2790 <rorifloris78@gmail.com>
2025-03-24 06:34:06 +0000

    feat: add update password endpoint for update password of account setting

--- src/routes/setting.route.js
+++ src/routes/setting.route.js
@@ -3,6 +3,7 @@ const {
   setSetting,
   getSetting,
   accountSetting,
+  changePassword,
 } = require("../controllers/setting.controller");
 const router = express.Router();
 const verifyToken = require("../middlewares/authJWT");
@@ -11,5 +12,6 @@ router.post("/set_setting/:id", verifyToken, setSetting);
 router.get("/get_setting", verifyToken, getSetting);
 
 router.post("/account_setting/:id", verifyToken, accountSetting);
+router.post("/update_password/:id", verifyToken, changePassword);
 
 module.exports = router;
