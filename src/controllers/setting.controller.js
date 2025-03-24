6a111be09d6d4880f596c80e8bbc71cfae2d53dd
luckyboy2790 <rorifloris78@gmail.com>
2025-03-24 05:42:42 +0000

    feat: add endpoint for Setting User Information of Account Setting

--- src/controllers/setting.controller.js
+++ src/controllers/setting.controller.js
@@ -1,13 +1,22 @@
-const SettingModel = require('../models/setting.model');
+const SettingModel = require("../models/setting.model");
+const EmployeeModel = require("../models/employee.model");
+const UserModel = require("../models/user.model");
 
 exports.setSetting = async (req, res) => {
   try {
     console.log(req.body);
-    const { country, timezone, timeFormat, rfidClock, timeInComments, ipRestriction } = req.body;
+    const {
+      country,
+      timezone,
+      timeFormat,
+      rfidClock,
+      timeInComments,
+      ipRestriction,
+    } = req.body;
 
     const { id } = req.params;
 
-    if (id && id !== 'undefined') {
+    if (id && id !== "undefined") {
       await SettingModel.findByIdAndUpdate(id, {
         country,
         timezone,
@@ -29,7 +38,7 @@ exports.setSetting = async (req, res) => {
       await settingModel.save();
     }
 
-    res.status(200).json({ message: 'Create successfully' });
+    res.status(200).json({ message: "Create successfully" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
@@ -40,7 +49,51 @@ exports.getSetting = async (req, res) => {
   try {
     const settingData = await SettingModel.find({});
 
-    res.status(200).json({ message: 'Fetch data successfully', settingData: settingData[0] });
+    res.status(200).json({
+      message: "Fetch data successfully",
+      settingData: settingData[0],
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.accountSetting = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { first_name, last_name, email } = req.body;
+
+    const { id } = req.params;
+
+    const userData = await UserModel.findById(id);
+
+    const user = await UserModel.findByIdAndUpdate(id, {
+      email: email,
+    });
+
+    const userEmployeeId = userData.employee;
+
+    console.log(userData);
+
+    const employee = await EmployeeModel.findByIdAndUpdate(userEmployeeId, {
+      first_name: first_name,
+      last_name: last_name,
+      full_name: `${first_name} ${last_name}`,
+      email: email,
+    });
+
+    const userResponse = {
+      _id: id,
+      email: email,
+      full_name: `${first_name} ${last_name}`,
+      account_type: user.account_type,
+      img: employee?.img || null,
+    };
+
+    res
+      .status(200)
+      .json({ message: "Create successfully", user: userResponse });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
