e084b8957548106fcf52a66de66633387e7198b7
luckyboy2790 <rorifloris78@gmail.com>
2025-04-11 14:53:15 +0100

    fix: remove all console logs

--- src/config/connect.js
+++ src/config/connect.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 exports.connectDB = () => {
   try {
     mongoose.connect(process.env.MONGO_URI);
-    console.log(process.env.MONGO_URI);
-
     console.log("connected to db");
   } catch (error) {
     handleError(error);
